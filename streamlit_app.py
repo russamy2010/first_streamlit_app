39c18330bc87716cb977f4b986a7c4639393ff2e
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 15:04:50 -0400

    Update streamlit_app.py
    updated syntax at line 38

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,9 @@ try:
     streamlit.write('The user entered ', fruit_choice)
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-    fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
- except URLError as e:
+except URLError as e:
   streamlit.error()
 
 # makes json version of response easier to read (normalize)
