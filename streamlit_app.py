51c0d2bbf4e3bb2fe315ecafda540615de8a1665
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 15:02:15 -0400

    Update streamlit_app.py
    added structure to separate code loaded once from code that is repeated.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,16 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice: 
+    streamlit.write('The user entered ', fruit_choice)
+  else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+ except URLError as e:
+  streamlit.error()
 
 # makes json version of response easier to read (normalize)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
