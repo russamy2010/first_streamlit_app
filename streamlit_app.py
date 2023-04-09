0b752f0b13477853c390ff93a527ddae7a6eacc4
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 14:55:55 -0400

    Update streamlit_app.py
    added in stop command
    organized code for control and imported error library for troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents\' New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -10,7 +15,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +30,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 
@@ -34,7 +37,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output of above line is a table
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+
+# don't run anything past here while troubleshooting
+streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -54,3 +60,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit=streamlit.text_input('What fruit would you like add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list_values ('from streamlit'))")
