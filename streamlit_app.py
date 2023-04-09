61fcab9ebe8c8e760061b39b93ccd09f07574b28
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 15:17:20 -0400

    Update streamlit_app.py
    created function and commented lines 42-44 out; if function works will delete those lines
    removed previous commented lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+3create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
@@ -32,16 +38,14 @@ try:
   if not fruit_choice: 
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
+    #streamlit.dataframe(back_from_function)
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
-# makes json version of response easier to read (normalize)
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output of above line is a table
-# streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while troubleshooting
 streamlit.stop()
