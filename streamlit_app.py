67e7642ecb3e79c3d5b99f2e58e7e6c42fc3c2df
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 10:42:34 -0400

    Update streamlit_app.py
    added line to make json version of response easier to read
    added line so that output is a table
    removed raw json line from streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
+
+# makes json version of response easier to read (normalize)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output of above line is a table
+streamlit.dataframe(fruityvice_normalized)
