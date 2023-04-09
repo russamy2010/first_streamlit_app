ab03e1096c927f4d67c4ca6b50dd233729dddde8
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 15:36:28 -0400

    Update streamlit_app.py
    removed lines 57-62
    #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    #my_cur = my_cnx.cursor()
    #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
    #my_data_row = get_fruit_load_list()
    #streamlit.text("Hello from Snowflake:")
    #streamlit.text(my_data_row)
    
    removed liens 59-60, 62
    #my_cur = my_cnx.cursor()
    #my_cur.execute("select * from fruit_load_list")
    #streamlit.header("The fruit load list contains:")

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,6 @@ except URLError as e:
 
 
 
-
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -54,18 +53,10 @@ def get_fruit_load_list():
        return my_cur.fetchall()
 #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = get_fruit_load_list()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
+
 
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
   my_data_rows = get_fruit_load_list()
-#streamlit.header("The fruit load list contains:")
   streamlit.dataframe(my_data_rows)
 
 # don't run anything past here while troubleshooting
