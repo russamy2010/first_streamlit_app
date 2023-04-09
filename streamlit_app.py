8dd39ba230157c69e8af07b3bf55dba394ac33a5
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 15:20:32 -0400

    Update streamlit_app.py
    updated line 45 with correct code

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
     #streamlit.dataframe(back_from_function)
     #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
