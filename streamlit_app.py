1fc41e9955beca05bd80bbe187240f9335a9a800
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 15:33:19 -0400

    Update streamlit_app.py
    removed quotations in line 56

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ def get_fruit_load_list():
        my_cur.execute("select*from fruit_load_list")
        return my_cur.fetchall()
 #Add a button to load the fruit
-if streamlit.button('Get Fruit Load List')"
+if streamlit.button('Get Fruit Load List')
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
