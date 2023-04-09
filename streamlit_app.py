edcc119c28c75c18b8306086797888bc83d01103
russamy2010 <129683994+russamy2010@users.noreply.github.com>
2023-04-09 15:23:19 -0400

    Update streamlit_app.py
    removed original lines 42-44: 
    
    #streamlit.dataframe(back_from_function)
        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
        #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,6 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
-    #streamlit.dataframe(back_from_function)
-    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
